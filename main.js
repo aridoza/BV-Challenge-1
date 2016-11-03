6a8bc4b776bffb56f7d632d1910447abb67a006e
aridoza <aridoza85@gmail.com>
2016-11-03 16:12:26 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// console.log("main.js loaded");
+
+const learnButton = document.querySelector('#learn-btn');
+learnButton.addEventListener('click', function() {
+  // console.log('learn click');
+  window.location = 'https://www.beenverified.com';
+});
+
+const learnButtonB = document.querySelector('#redef-learn-btn').addEventListener('click', function() {
+  window.location = 'https://www.beenverified.com';
+})
